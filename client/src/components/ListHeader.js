b231b46c137816f90b4e16577d5994eb91b72253
Benkalio <officialtennysonbk@gmail.com>
2023-02-03 14:36:39 +0100

    Style buttons and implemented js for editing tasks

--- client/src/components/ListHeader.js
+++ client/src/components/ListHeader.js
@@ -1,5 +1,8 @@
+import { useState } from "react";
+import Modal from "./Modal";
 
 const ListHeader = ({ listName }) => {
+  const [showModal, setShowModal] = useState(false);
 
   const signOut = () => {
     console.log('logout');
@@ -9,9 +12,10 @@ const ListHeader = ({ listName }) => {
     <div className="list-header">
       <h1>{listName}</h1>
       <div className="button-container">
-        <button className="create">ADD NEW</button>
-        <button className="logout" onClick={signOut}>SIGN OUT</button>
+        <button className="create" onClick={() => setShowModal(true)}>ADD NEW</button>
+        <button className="sign-out" onClick={signOut}>SIGN OUT</button>
       </div>
+      {showModal && <Modal mode={'create'} setShowModal={setShowModal} />}
     </div>
   );
 };
--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -1,5 +1,78 @@
-const Modal = () => {
-  return <div></div>;
+import { useState } from "react";
+
+const Modal = ({ mode, setShowModal, task }) => {
+  const editMode = mode === 'edit' ? true : false;
+
+  const [data, setData] = useState({
+    user_email: editMode ? task.user_email : null,
+    title: editMode ? task.title : null,
+    progress: editMode ? task.progress : 50,
+    data: editMode ? "" : new Date()
+  });
+
+  // SENDING DATA TO THE DATABASE
+  const postData = () => {
+    try {
+      
+    } catch (err) {
+      console.error(err);
+    }
+  }
+  //END OF SENDING DATA TO THE DATABASE
+
+  const handleChange = (e) => {
+    
+    const { name, value } = e.target;
+
+    setData({...data, [name]: value });
+
+    // or use this.handle
+    // setData(data => ({
+    //   ...data,
+    //   [name]: value
+    // }))
+
+    console.log(data);
+  };
+
+  return (
+    <div className="overlay">
+      <div className="modal">
+        <div className="form-title-container">
+          <h3>Let's {mode} your task.</h3>
+          <button
+            onClick={() => setShowModal(false)}
+            className="">
+            X
+          </button>
+        </div>
+        <form className="modal-form">
+          <input
+            type="text"
+            maxLength={30}
+            placeholder="Add task here" 
+            name="title"
+            value={data.title}
+            onChange={handleChange}
+            required
+          />
+          <br />
+          <label htmlFor="">Drag to update your progress</label>
+          <input
+            id="range"
+            type="range"
+            min="0"
+            max="100"
+            name="progress"
+            placeholder="Description"
+            value={data.progress}
+            onChange={handleChange}
+          />
+          <input className={mode} type="submit" />
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Modal;
